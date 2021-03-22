35d55d36ab9a24fdfb989d1819449cf22be1ed91
Rowan Kieboom <rkieboom@rowans-mbp.localdomain>
2021-03-23 00:13:16 +0100

    Added swap function

---
+++ ft_swap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_swap.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/03/22 21:45:19 by rkieboom      #+#    #+#                 */
+/*   Updated: 2021/03/22 23:06:40 by rkieboom      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+#include "stack_list/stack_list.h"
+
+void	ft_swap(int *num1, int *num2)
+{
+	int temp;
+
+	temp = *num2;
+	*num2 = *num1;
+	*num1 = temp;
+}
+
+void	ft_swapp(struct s_stack *p1, struct s_stack *p2)
+{
+	struct s_stack temp;
+	
+	temp = *p2;
+	*p2 = *p1;
+	*p1 = temp;
+}
