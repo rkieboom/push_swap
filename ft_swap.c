9eebf8e976278f09177a232a9141588c24066d70
Rowan Kieboom <rkieboom@rowans-mbp.localdomain>
2021-04-06 04:54:22 +0200

    added comment for later

--- ft_swap.c
+++ ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/22 21:45:19 by rkieboom      #+#    #+#                 */
-/*   Updated: 2021/03/22 23:06:40 by rkieboom      ########   odam.nl         */
+/*   Updated: 2021/04/06 00:53:12 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	ft_swap(int *num1, int *num2)
 	*num1 = temp;
 }
 
+//Misschien weghalen
 void	ft_swapp(struct s_stack *p1, struct s_stack *p2)
 {
 	struct s_stack temp;
