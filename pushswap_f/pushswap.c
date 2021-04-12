fa1e8f319a55899836457201efeca0ab6332b118
BuildTools <unconfigured@null.spigotmc.org>
2021-04-12 02:52:27 +0200

    added pushswap with simple sorting func

--- pushswap_f/pushswap.c
+++ pushswap_f/pushswap.c
@@ -6,11 +6,13 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/08 23:55:52 by rkieboom      #+#    #+#                 */
-/*   Updated: 2021/04/09 00:16:09 by rkieboom      ########   odam.nl         */
+/*   Updated: 2021/04/12 02:44:09 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
+
+void	print_list(t_stack *list);
 
 int		main(int argc, char **argv)
 {
@@ -23,5 +25,10 @@ int		main(int argc, char **argv)
 	stack_a = save_stack(argv, NULL);
 	if (!stack_a)
 		return (-1);
+
+	sort(&stack_a, &stack_b);
+	print_list(stack_a);
+	printf("\n\n\n-----\n\n\n");
+	print_list(stack_b);
 	return (0);
 }
--- pushswap_f/pushswap.h
+++ pushswap_f/pushswap.h
@@ -6,19 +6,22 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/08 23:54:47 by rkieboom      #+#    #+#                 */
-/*   Updated: 2021/04/09 00:13:22 by rkieboom      ########   odam.nl         */
+/*   Updated: 2021/04/12 00:42:06 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSHSWAP_H
-# define PUSHSWAP_F
+# define PUSHSWAP_H
 
 # include "../commands/commands.h"
 # include "../libft/libft.h"
 # include "../stack_list/stack_list.h"
 # include "../get_next_line/get_next_line.h"
 
+int		sort(t_stack **stack_a, t_stack **stack_b);
+
 t_stack	*save_stack(char **argv, t_stack *list);
+void	set_min_max(int *min_max, t_stack *stack_a, t_stack *stack_b);
 void	ft_reterror(void);
 
 #endif
